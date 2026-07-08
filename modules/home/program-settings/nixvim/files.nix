aa6cce09eda7324ce5aa441396cbfe982e2daa34
water2027 <136900643+water2027@users.noreply.github.com>
2026-07-08 20:03:03 +0800

    Update files.nix

--- modules/home/program-settings/nixvim/files.nix
+++ modules/home/program-settings/nixvim/files.nix
@@ -1,3 +1,32 @@
+let
+  copyPathAction =
+    {
+      absolute ? false,
+      withLine ? false,
+      label,
+    }:
+    ''
+      function()
+        local absolute_path = vim.fn.expand("%:p")
+        if absolute_path == "" then
+          vim.notify("No file path to copy", vim.log.levels.WARN)
+          return
+        end
+
+        local path = absolute_path
+        if not ${if absolute then "true" else "false"} then
+          path = vim.fs.relpath(vim.fn.getcwd(), absolute_path) or vim.fn.fnamemodify(absolute_path, ":.")
+        end
+
+        if ${if withLine then "true" else "false"} then
+          path = path .. ":" .. vim.api.nvim_win_get_cursor(0)[1]
+        end
+
+        vim.fn.setreg("+", path)
+        vim.notify("Copied ${label}: " .. path)
+      end
+    '';
+in
 {
   programs.nixvim = {
     plugins = {
@@ -27,19 +56,38 @@
       {
         mode = "n";
         key = "<leader>yp";
-        action.__raw = ''
-          function()
-            local path = vim.fn.expand("%:p")
-            if path == "" then
-              vim.notify("No file path to copy", vim.log.levels.WARN)
-              return
-            end
-
-            vim.fn.setreg("+", path)
-            vim.notify("Copied file path: " .. path)
-          end
-        '';
-        options.desc = "Copy file path";
+        action.__raw = copyPathAction {
+          label = "relative path";
+        };
+        options.desc = "Copy relative path";
+      }
+      {
+        mode = "n";
+        key = "<leader>yP";
+        action.__raw = copyPathAction {
+          absolute = true;
+          label = "absolute path";
+        };
+        options.desc = "Copy absolute path";
+      }
+      {
+        mode = "n";
+        key = "<leader>yl";
+        action.__raw = copyPathAction {
+          withLine = true;
+          label = "relative path with line";
+        };
+        options.desc = "Copy relative path with line";
+      }
+      {
+        mode = "n";
+        key = "<leader>yL";
+        action.__raw = copyPathAction {
+          absolute = true;
+          withLine = true;
+          label = "absolute path with line";
+        };
+        options.desc = "Copy absolute path with line";
       }
     ];
   };
