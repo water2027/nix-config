dc585797c35d7dbe6a7b072dac64f643d3a08293
water <136900643+water2027@users.noreply.github.com>
2026-07-16 01:28:21 +0800

    zsh

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -18,6 +18,12 @@
   };
   programs.zsh = {
     enableCompletion = true;
+    completionInit = ''
+      # Keep Zim-like substring matching, e.g. ".md" completes "123.md".
+      zstyle ':completion:*' matcher-list "" "m:{a-zA-Z}={A-Za-z}" "r:|[._-]=* r:|=*" "l:|=* r:|=*"
+
+      autoload -U compinit && compinit
+    '';
     autocd = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
